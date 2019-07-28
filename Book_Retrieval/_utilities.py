bfb744c6440145c94c74c7be0bf9ff2a95baa28c
sidtyagi <mailsiddharthtyagi@gmail.com>
2019-07-28 21:08:36 +0530

    Added book retrieval

---
+++ Book_Retrieval/_utilities.py
@@ -0,0 +1,17 @@
+import pickle
+
+books =[
+    {
+        'name': "AAA",
+        'price': "100",
+        'isbn': "111111"
+    },
+    {
+        'name': "BBB",
+        'price': "200",
+        'isbn': "222222"
+    }
+]
+
+def get_available_books():
+    return books
